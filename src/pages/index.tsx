e7ddb02db9c87f625746ebba2ccc0282a60a2229
carol <carolbrandes23@gmail.com>
2023-09-26 06:30:55 -0300

    use High Order function for reuse withSSRGuest in more than one component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
 import { FormEvent, useContext, useState } from "react";
 import styles from "../styles/Home.module.css";
 import { AuthContext } from "@/contexts/AuthContext";
-import { GetServerSideProps } from "next";
-import { parseCookies } from "nookies";
+
+import { withSSRGuest } from "@/utils/withSSRGuest";
 
 export default function Home() {
   const [email, setEmail] = useState("");
@@ -37,19 +37,8 @@ export default function Home() {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const cookies = parseCookies(ctx);
-
-  if (cookies["nextauth.token"]) {
-    return {
-      redirect: {
-        destination: "/dashboard",
-        permanent: false,
-      },
-    };
-  }
-
+export const getServerSideProps = withSSRGuest(async (ctx) => {
   return {
     props: {},
   };
-};
+});
